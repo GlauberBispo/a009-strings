346bf31caafd294a80b0f095ccc0cbb3cca06adc
GlauberBispo <glauber.bispo@gmail.com>
2022-07-26 19:38:05 -0300

    Entrega exercicio3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
 //Crie a const para a frase aqui
+
+const casaDeJorge = `Jorge tem uma casa verde e com portão\n azul, com os dizeres: \"BOAS VINDAS, mas\n não deixe o gato sair\"`
+console.log(casaDeJorge)
+const mudancaDeCor = casaDeJorge.replace("verde","rosa");
+console.log(mudancaDeCor)
+const mudancaDeCor2 = mudancaDeCor.replace("azul","laranja");
+console.log(mudancaDeCor2);
+console.log(`A nova string tem a palavra verde?: ${mudancaDeCor2.includes("verde")}\nA nova string possui a palavra laranja? ${mudancaDeCor2.includes("laranja")}`);
+const gatoMaiusculo = mudancaDeCor2.replace("mas","MAS").replace("não", "NÃO").replace("deixe","DEIXE").replace("o gato sair","O GATO SAIR");
+console.log(gatoMaiusculo);
