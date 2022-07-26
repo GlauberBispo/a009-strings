68a83f4e5c199f3ef0703f2cc3708a84065f246a
GlauberBispo <glauber.bispo@gmail.com>
2022-07-26 19:37:49 -0300

    Entrega exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,7 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString);
+console.log(`A string tem ${minhaString.length} caracteres`);
+const novaString = minhaString.trim();
+console.log(`Após retirar os espaços inúteis temos ${novaString.length} caracteres.`);
+const stringSticioso = novaString.replaceAll("________", "sticioso");
+console.log (`Substituindo os \"________\" por sticioso fica:\n ${stringSticioso}.`)
